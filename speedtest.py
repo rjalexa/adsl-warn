45e0ef7b8b12d3d6922d3938f089db3a72049a2e
rjalexa <gogonegro@gmail.com>
2019-12-09 13:16:14 +0100

    Started refactoring monolithic program into main and functions: perform measurement and send email

--- speedtest.py
+++ speedtest.py
@@ -1,38 +1,50 @@
 """docstring."""
 import subprocess
 import json
-import dateutil.parser as dparser
 import smtplib
+import dateutil.parser as dparser
 
-sender = "SpeedTest Monitor <noreply@gmail.com>"
-receiver = "Robert Alexander <gogonegro@gmail.com>"
+SENDER = "SpeedTest Monitor <noreply@gmail.com>"
+RECEIVER = "Robert Alexander <gogonegro@gmail.com>"
+SMTP_USER = "86aa40c0e245e2"
+SMTP_PASS = "827278518085d6"
 
-process = subprocess.Popen(['/usr/local/bin/speedtest', '-f', 'json'],
-                           stdout=subprocess.PIPE,
-                           stderr=subprocess.PIPE)
+def st_json():
+    """Run Ookla's speedtest and return JSON data"""
+    process = subprocess.Popen(['/usr/local/bin/speedtest', '-f', 'json'],
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    json_data = json.loads(stdout)
+    return json_data
 
-stdout, stderr = process.communicate()
-json_data = json.loads(stdout)
+def send_msg():
+    """Send an email to desired server with speedtest warning"""
+    message = f"""\
+    Subject: ADSL warning {testtime} UTC
+    To: {RECEIVER}
+    From: {RECEIVER}
 
-testtime = dparser.parse(json_data["timestamp"])\
-           .strftime("%A, %-d %b %Y at %H:%M:%S")
+    Your {json_data["isp"]} ADSL line performance is currently
+    below the threshold.
 
-message = f"""\
-Subject: ADSL warning {testtime} UTC
-To: {receiver}
-From: {sender}
+    Download: {json_data["download"]["bandwidth"]/124950:3.1f} Mbps
+    Upload  : {json_data["upload"]["bandwidth"]/124950:2.1f} Mbps
 
-Your {json_data["isp"]} ADSL line performance is currently
-below the threshold.
+    Tested on {json_data["server"]["name"]} server with
+    {json_data["packetLoss"]:3.3f} packet loss and {json_data["ping"]["latency"]} ping.
+    Timestamp (UTC): {json_data["timestamp"]}
+    """
+    with smtplib.SMTP("smtp.mailtrap.io", 2525) as server:
+        server.login(SMTP_USER, SMTP_PASS)
+        server.sendmail(SENDER, RECEIVER, message)
 
-Download: {json_data["download"]["bandwidth"]/124950:3.1f} Mbps
-Upload  : {json_data["upload"]["bandwidth"]/124950:2.1f} Mbps
+def main():
+    """Main program."""
+    json_data = st_json()
+    testtime = dparser.parse(json_data["timestamp"])\
+               .strftime("%A, %-d %b %Y at %H:%M:%S")
+    print(f"{testtime}: {json_data}")
 
-Tested on {json_data["server"]["name"]} server with
-{json_data["packetLoss"]:3.3f} packet loss and {json_data["ping"]["latency"]} ping.
-Timestamp (UTC): {json_data["timestamp"]}
-"""
-
-with smtplib.SMTP("smtp.mailtrap.io", 2525) as server:
-    server.login("86aa40c0e245e2", "827278518085d6")
-    server.sendmail(sender, receiver, message)
+if __name__ == "__main__":
+    main()

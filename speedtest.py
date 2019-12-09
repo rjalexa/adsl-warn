19121b7f2f020633a938bea6ea8b06cd7d43d8fd
rjalexa <gogonegro@gmail.com>
2019-12-09 16:33:44 +0100

    Some pylama corrections

--- speedtest.py
+++ speedtest.py
@@ -6,11 +6,12 @@ import dateutil.parser as dparser
 
 SENDER = "SpeedTest Monitor <noreply@gmail.com>"
 RECEIVER = "Robert Alexander <gogonegro@gmail.com>"
-SMTP_USER = "86aa40c0e245e2" # Your mailtrap.io userid
-SMTP_PASS = "827278518085d6" # Your mailtrap.io password
+SMTP_USER = "86aa40c0e245e2"  # Your mailtrap.io userid
+SMTP_PASS = "827278518085d6"  # Your mailtrap.io password
+
 
 def st_json():
-    """Run Ookla's speedtest and return JSON data"""
+    """Run Ookla's speedtest and return JSON data."""
     process = subprocess.Popen(['/usr/local/bin/speedtest', '-f', 'json'],
                                stdout=subprocess.PIPE,
                                stderr=subprocess.PIPE)
@@ -18,8 +19,9 @@ def st_json():
     json_data = json.loads(stdout)
     return json_data
 
+
 def send_msg(testtime, json_data):
-    """Send an email to desired server with speedtest warning"""
+    """Send an email to desired server with speedtest warning."""
     message = f"""\
     Subject: ADSL warning {testtime} UTC
     To: {RECEIVER}
@@ -36,17 +38,18 @@ def send_msg(testtime, json_data):
     Timestamp (UTC): {testtime}
     """
     print(f'{testtime} UTC - {json_data["isp"]} -\
-            {json_data["download"]["bandwidth"]/124950:3.1f} Mbps') # DEBUG
+            {json_data["download"]["bandwidth"]/124950:3.1f} Mbps')  # DEBUG
     with smtplib.SMTP("smtp.mailtrap.io", 2525) as server:
         server.login(SMTP_USER, SMTP_PASS)
         server.sendmail(SENDER, RECEIVER, message)
 
+
 def main():
-    """Main program."""
+    """Run a speedtest and send email warning if under threshold."""
     j_d = st_json()
-    testtime = dparser.parse(j_d["timestamp"])\
-               .strftime("%A, %-d %b %Y at %H:%M:%S")
+    testtime = dparser.parse(j_d["timestamp"]).strftime("%A, %-d %b %Y at %H:%M:%S")
     send_msg(testtime, j_d)
+
 
 if __name__ == "__main__":
     main()

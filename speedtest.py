0455ef5d22bb05ecaf36e49a0e3e3c4e7cda916d
rjalexa <gogonegro@gmail.com>
2019-12-09 13:50:47 +0100

    added debug statement to send_msg routine

--- speedtest.py
+++ speedtest.py
@@ -7,7 +7,7 @@ import dateutil.parser as dparser
 SENDER = "SpeedTest Monitor <noreply@gmail.com>"
 RECEIVER = "Robert Alexander <gogonegro@gmail.com>"
 SMTP_USER = "86aa40c0e245e2" # Your mailtrap.io userid
-SMTP_PASS = "827278518085d6" # Your mailtrap.io password 
+SMTP_PASS = "827278518085d6" # Your mailtrap.io password
 
 def st_json():
     """Run Ookla's speedtest and return JSON data"""
@@ -18,7 +18,7 @@ def st_json():
     json_data = json.loads(stdout)
     return json_data
 
-def send_msg():
+def send_msg(testtime, json_data):
     """Send an email to desired server with speedtest warning"""
     message = f"""\
     Subject: ADSL warning {testtime} UTC
@@ -33,18 +33,19 @@ def send_msg():
 
     Tested on {json_data["server"]["name"]} server with
     {json_data["packetLoss"]:3.3f} packet loss and {json_data["ping"]["latency"]} ping.
-    Timestamp (UTC): {json_data["timestamp"]}
+    Timestamp (UTC): {testtime}
     """
+    print(f'{testtime} UTC - {json_data["isp"]} - {json_data["download"]["bandwidth"]/124950:3.1f} Mbps') # DEBUG 
     with smtplib.SMTP("smtp.mailtrap.io", 2525) as server:
         server.login(SMTP_USER, SMTP_PASS)
         server.sendmail(SENDER, RECEIVER, message)
 
 def main():
     """Main program."""
-    json_data = st_json()
-    testtime = dparser.parse(json_data["timestamp"])\
+    jd = st_json()
+    testtime = dparser.parse(jd["timestamp"])\
                .strftime("%A, %-d %b %Y at %H:%M:%S")
-    print(f"{testtime}: {json_data}")
+    send_msg(testtime, jd)
 
 if __name__ == "__main__":
     main()
